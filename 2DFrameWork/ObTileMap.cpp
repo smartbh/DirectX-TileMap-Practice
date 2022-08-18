19b199999c660a17483a24c2bda810869380733f
smartbh <smartbh@naver.com>
2022-08-19 04:30:57 +0900

    초기 커밋.

---
+++ 2DFrameWork/ObTileMap.cpp
@@ -0,0 +1,169 @@
+#include "framework.h"
+
+ObTileMap::ObTileMap()
+{
+    SafeRelease(vertexBuffer);
+    SafeDeleteArray(vertices);
+    for (int i = 0; i < 4; i++)
+        SafeDelete(tileImages[i]);
+
+    tileSize.x = 20;
+    tileSize.y = 20;
+
+    file = "map1.txt";
+    scale = Vector2(50.0f, 50.0f);
+
+    tileImages[0] = new ObImage(L"Tile.png");
+    tileImages[0]->maxFrame = Int2(8, 6);
+    tileImages[1] = new ObImage(L"Tile2.png");
+    tileImages[1]->maxFrame = Int2(11, 7);
+    tileImages[2] = new ObImage(L"pepe.png");
+    tileImages[3] = nullptr;
+
+    ResizeTile(tileSize);
+
+}
+
+ObTileMap::~ObTileMap()
+{
+    SafeRelease(vertexBuffer);
+    SafeDeleteArray(vertices);
+
+    for (int i = 0; i < 4; i++)
+        SafeDelete(tileImages[i]);
+}
+
+void ObTileMap::Render()
+{
+    if (!visible)return;
+
+    GameObject::Render();
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (tileImages[i])
+        {
+            D3D->GetDC()->PSSetShaderResources(i, 1, &tileImages[i]->SRV);
+            D3D->GetDC()->PSSetSamplers(i, 1, &tileImages[i]->sampler);
+        }
+    }
+
+    tileMapShader->Set();
+
+    UINT stride = sizeof(VertexTile);
+    UINT offset = 0;
+
+    //버텍스버퍼 바인딩
+    D3D->GetDC()->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+    D3D->GetDC()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    D3D->GetDC()->Draw(tileSize.x * tileSize.y * 6, 0);
+}
+
+void ObTileMap::SetTile(Int2 TileIdx, Int2 FrameIdx, int ImgIdx, int TileState, Color color)
+{
+    int tileIdx = tileSize.x * TileIdx.y + TileIdx.x;
+
+    vertices[tileIdx * 6 + 0].uv.x = FrameIdx.x / (float)tileImages[ImgIdx]->maxFrame.x;
+    vertices[tileIdx * 6 + 1].uv.x = FrameIdx.x / (float)tileImages[ImgIdx]->maxFrame.x;
+    vertices[tileIdx * 6 + 5].uv.x = FrameIdx.x / (float)tileImages[ImgIdx]->maxFrame.x;
+
+    vertices[tileIdx * 6 + 2].uv.x = (FrameIdx.x + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.x;
+    vertices[tileIdx * 6 + 3].uv.x = (FrameIdx.x + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.x;
+    vertices[tileIdx * 6 + 4].uv.x = (FrameIdx.x + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.x;
+
+    vertices[tileIdx * 6 + 3].uv.y = FrameIdx.y / (float)tileImages[ImgIdx]->maxFrame.y;
+    vertices[tileIdx * 6 + 1].uv.y = FrameIdx.y / (float)tileImages[ImgIdx]->maxFrame.y;
+    vertices[tileIdx * 6 + 5].uv.y = FrameIdx.y / (float)tileImages[ImgIdx]->maxFrame.y;
+
+    vertices[tileIdx * 6 + 2].uv.y = (FrameIdx.y + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.y;
+    vertices[tileIdx * 6 + 0].uv.y = (FrameIdx.y + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.y;
+    vertices[tileIdx * 6 + 4].uv.y = (FrameIdx.y + 1.0f) / (float)tileImages[ImgIdx]->maxFrame.y;
+
+    for (int i = 0; i < 6; i++)
+    {
+        vertices[tileIdx * 6 + i].tileMapIdx = ImgIdx;
+        vertices[tileIdx * 6 + i].color = color;
+        vertices[tileIdx * 6 + i].tileState = TileState;
+    }
+    
+    //갱신
+    D3D->GetDC()->UpdateSubresource(vertexBuffer, 0, NULL, vertices, 0, 0);
+}
+
+void ObTileMap::ResizeTile(Int2 TileSize)
+{
+    VertexTile* Vertices = new VertexTile[TileSize.x * TileSize.y * 6];
+
+    //세로
+    for (int i = 0; i < TileSize.y; i++)
+    {
+        //가로
+        for (int j = 0; j < TileSize.x; j++)
+        {
+            int tileIdx = TileSize.x * i + j;
+
+            //0
+            Vertices[tileIdx * 6].position.x = 0.0f + j;
+            Vertices[tileIdx * 6].position.y = 0.0f + i;
+            Vertices[tileIdx * 6].uv = Vector2(0.0f, 1.0f);
+            //1                             
+            Vertices[tileIdx * 6 + 1].position.x = 0.0f + j;
+            Vertices[tileIdx * 6 + 1].position.y = 1.0f + i;
+            Vertices[tileIdx * 6 + 1].uv = Vector2(0.0f, 0.0f);
+            //2                             
+            Vertices[tileIdx * 6 + 2].position.x = 1.0f + j;
+            Vertices[tileIdx * 6 + 2].position.y = 0.0f + i;
+            Vertices[tileIdx * 6 + 2].uv = Vector2(1.0f, 1.0f);
+            //3
+            Vertices[tileIdx * 6 + 3].position.x = 1.0f + j;
+            Vertices[tileIdx * 6 + 3].position.y = 1.0f + i;
+            Vertices[tileIdx * 6 + 3].uv = Vector2(1.0f, 0.0f);
+            //4
+            Vertices[tileIdx * 6 + 4].position.x = 1.0f + j;
+            Vertices[tileIdx * 6 + 4].position.y = 0.0f + i;
+            Vertices[tileIdx * 6 + 4].uv = Vector2(1.0f, 1.0f);
+            //5
+            Vertices[tileIdx * 6 + 5].position.x = 0.0f + j;
+            Vertices[tileIdx * 6 + 5].position.y = 1.0f + i;
+            Vertices[tileIdx * 6 + 5].uv = Vector2(0.0f, 0.0f);
+        }
+    }
+
+    //Copy
+    if (vertices)
+    {
+        Int2 Min = Int2(min(TileSize.x, tileSize.x), min(TileSize.y, tileSize.y));
+        for (int i = 0; i < Min.y; i++)
+        {
+            for (int j = 0; j < Min.x; j++)
+            {
+                int SrcIdx = tileSize.x * i + j;
+                int DestIdx = TileSize.x * i + j;
+                for (int k = 0; k < 6; k++)
+                {
+                    Vertices[DestIdx * 6 + k] = vertices[SrcIdx * 6 + k];
+                }
+            }
+        }
+    }
+
+    SafeDeleteArray(vertices);
+    vertices = Vertices;
+    tileSize = TileSize;
+    SafeRelease(vertexBuffer);
+
+    //CreateVertexBuffer
+    {
+        D3D11_BUFFER_DESC desc;
+        desc = { 0 };
+        desc.Usage = D3D11_USAGE_DEFAULT;
+        desc.ByteWidth = sizeof(VertexTile) * tileSize.x * tileSize.y * 6;
+        desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+        D3D11_SUBRESOURCE_DATA data = { 0 };
+        data.pSysMem = vertices;
+        HRESULT hr = D3D->GetDevice()->CreateBuffer(&desc, &data, &vertexBuffer);
+        Check(hr);
+    }
+}
+
+
