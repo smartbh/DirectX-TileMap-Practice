000c0ef41f01d8ead6f74601c2f63fb9a146a04c
smartbh <smartbh@naver.com>
2022-08-19 16:38:26 +0900

    타일맵 꾸며보기

--- 2DFrameWork/ObTileMap.h
+++ 2DFrameWork/ObTileMap.h
@@ -14,12 +14,12 @@ class ObTileMap : public GameObject
 protected:
     VertexTile*         vertices;
     ID3D11Buffer*       vertexBuffer;
-    Int2                tileSize;   //10x10 , 4x4
+    
 
 public:
     ObImage*            tileImages[4];
     string              file;
-
+    Int2                tileSize;   //10x10 , 4x4
 public:
     ObTileMap();
     virtual ~ObTileMap();
@@ -29,5 +29,6 @@ public:
     virtual void        ResizeTile(Int2 TileSize);
     void                SetTile(Int2 TileIdx, Int2 FrameIdx, int ImgIdx = 0,
         int TileState = TILE_NONE, Color color = Color(0.5f, 0.5f, 0.5f, 0.5f));
+
 };
 
--- Game1/Scene01.cpp
+++ Game1/Scene01.cpp
@@ -6,9 +6,86 @@ void Scene01::Init()
 	mon = new Monster();
 	map = new ObTileMap();
 	map->SetWorldPos(Vector2(-app.GetHalfWidth(), -app.GetHalfHeight()));
+	
+	//0 = 사막맵, 1 = tile2.png 파일
 
-	map->SetTile(Int2(0, 0), Int2(2 ,2), 1);
-	map->SetTile(Int2(1, 1), Int2(0 ,0), 2);
+	//가운데 도로 만들기
+	for (int i = 0; i < map->tileSize.x; i++)
+	{
+		for(int j = 0; j < map->tileSize.y; j++)
+		{
+			map->SetTile(Int2(i, j), Int2(5, 3), 0);//모래로 한번 채우고
+
+			map->SetTile(Int2(9, j), Int2(1, 4), 0); //도로 십자로 만들기
+			map->SetTile(Int2(10, j), Int2(1, 4), 0); //세로
+			map->SetTile(Int2(8, j), Int2(0, 4), 0); //도로 양옆테두리
+			map->SetTile(Int2(11, j), Int2(2, 4), 0); 
+
+			map->SetTile(Int2(j, 9), Int2(1, 4), 0); //도로 십자로 만들기
+			map->SetTile(Int2(j, 10), Int2(1, 4), 0); //가로
+			map->SetTile(Int2(j, 8), Int2(1, 5), 0); //도로 양옆테두리
+			map->SetTile(Int2(j, 11), Int2(1, 3), 0);
+
+		}
+	}
+
+	//8,9,10,11
+	for (int i = 8; i < 12; i++)
+	{
+		for (int j = 8; j < 12; j++)
+		{
+			map->SetTile(Int2(i, j), Int2(1, 4), 0);
+		}
+	}
+
+	//마지막 네 귀퉁이,
+	map->SetTile(Int2(8, 11), Int2(4, 5), 0); //좌 상
+	map->SetTile(Int2(8, 8), Int2(4, 4), 0); //좌 하
+	map->SetTile(Int2(11, 11), Int2(3, 5), 0); //우 상
+	map->SetTile(Int2(11, 8), Int2(3, 4), 0); //우 하
+
+	///도로 끝
+
+	//사물 배치
+	map->SetTile(Int2(7, 7), Int2(5, 5), 0); //표지판
+
+	
+	
+	
+
+	//길뚫기
+	map->SetTile(Int2(3, 11), Int2(6, 1), 0);
+	map->SetTile(Int2(4, 11), Int2(6, 1), 0);
+	map->SetTile(Int2(3, 12), Int2(6, 1), 0);
+	map->SetTile(Int2(4, 12), Int2(6, 1), 0);
+
+	map->SetTile(Int2(3, 13), Int2(6, 0), 0);
+	map->SetTile(Int2(4, 13), Int2(6, 0), 0);
+
+	//집만들기 2~5, 18~15(위에서 아래)
+	for (int i = 2; i < 6; i++)//가로
+	{
+		for (int j = 15; j < 19; j++)//세로
+		{
+			map->SetTile(Int2(i, j), Int2(1, 1), 0); //집 토대
+
+			map->SetTile(Int2(2, j), Int2(0, 1), 0); //왼쪽벽
+			map->SetTile(Int2(5, j), Int2(2, 1), 0); //오른쪽쪽벽
+
+			map->SetTile(Int2(i, 18), Int2(1, 0), 0); //윗쪽벽
+			map->SetTile(Int2(i, 15), Int2(1, 2), 0); //아래벽
+		}
+	}
+	
+	//집 테두리
+	map->SetTile(Int2(2,18), Int2(0,0), 0); //좌 상
+	map->SetTile(Int2(2,15), Int2(0,2), 0);//좌 하
+	map->SetTile(Int2(5,18), Int2(2,0), 0);//우 상
+	map->SetTile(Int2(5,15), Int2(2,2), 0);//우 하
+
+	//집 주변 꾸미기
+
+
 }
 
 void Scene01::Release()
